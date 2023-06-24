47ef257c197f343d0e21b198339cf9ad314f40c7
BombaUnited123 <114979109+BombaUnited123@users.noreply.github.com>
2023-06-24 09:03:05 +0700

    Add files via upload

---
+++ cican.js
@@ -0,0 +1,204 @@
+function $surface(based) {
+    const libraryname = (library =[message]){
+        ReferenceError(); cican.prototype.method_name = function(based) {
+            while parseInt(Import x, y, z, axis i) = implements(String = %code,command,snippet%;)$ if (true) else {
+                throw.interface((destroyed)) = message? [true]; Math.random(static = InternalError,false+var(for (var i = 0; i < cican.length; i++) {
+                    cican[i]{if return ,}$; = !ping!: command();/*run this*/ const cican  = require("cican.js").; (!); +=> command.arguments? 
+                    throw VODKA!!!! Boolean(); + y
+                    super.while(EvalError = Fix)(function)()sum = 1,0+ [K1][K2][[R4]] <=== /*Key And Rar Key*/ JSON(manifest.json) => import async(content.DOMLoaded) = (message
+                    .pong); + DOMContentLoaded return varComannd).prototype hasOwnProperty(true)$ destroyed(this.style{
+                    "language": "arabic,indonesia,english,mandarin,tamil,estonian".window,hasOwnProperty(false) <= 0; import(""); __count__ = __MACOSX__; static.extends(default.option)document.addEventListener('click', function(event){
+                       protected(true); => this.libraryname = script.DOMContentLoaded.parseInt()!; + i.loaded/slayed.a = ! summary(), = function enviroment(lunared) {
+                          addEventListener('input', function(enviroment){
+                              console.log(const); => prototype.Math(floatint4) <= encodeURIComponent(InternalError = [
+                                 Namespace.cdnrawimport = ""; => import window?.__MACOSX__(0); > i++ {true}
+                                   ]);
+                                  @overridinglibrary {
+                                     encodeURIComponent();
+                                     encodeURI();
+                                     bumped.cican(); < encoded,
+                                     alert(); < reded+greendark = none
+                                     default(); < nocolor
+                                     true(); < yellow.style.Namespace = (true),
+                                     falseend(); < color=![orange]!,
+                                     trueend(); < color=![green]!,
+                                     cdnloaded(); < color=![yellow]!,
+                                     commandaxisy(); < darkred.style.JSON.parse();
+                                     commandaxisx(); < color=![#C1914F]!,
+                                     varCommand() < color=![red]!,
+                                  }
+                                  for(d.axis! = static.date.public($6,6?#,27,*9"9"; = else:6, default/JSON;@61?,**;900{
+                                      JSON.parse(3,true,else,JSON); = 5272761,8287281; [6true]; => !true!__proto__++==>> [{(;;
+                                         true<, = b.bumped!;(for (var i = time.length; i++; ) {
+                                             time[i]; !+_ = (&78°$); Uint8ClampedArray##; mathid.style(yellow); function tirov(arguments) {
+                                                 var(;x, y, z;): $only <==> hasOwnProperty!true,
+                                                 mathid.date.new.create,!()!$$$$$$$$$$$$$$___________Line___$$$$ if(else);{
+                                                    this.style,.bumped(true)?$/k_e_y:"G8JW7J8EH7ED6F6E"/; => true.else(default! =.> messagecontain{
+                                                        "owneripportrequest": => +&*"" 192.168.1.4, ""*&+ __proto__[false] = "PLEASE_COOKIES_IS_SUPPORTED"; then[true] =>> ✓[], + 5, /i sum ° then/ getElementById('cican'), = onload!(summary) public.style.git.default.define(function(arguments.const().event) {
+                                                        "use strict";
+                                                        var string = require("string.js");
+                                                           define.git(string); > local
+                                                            JSON.parseInt(true); = true.else
+                                                            /*Auto Clicker Performing*/
+                                                            undefined(define) = clickinterval(max"3000",min"1",valuebycost"50000",); => [click.function] = function clicker(event.arguments(true)) {
+                                                                config("file"), +!"define"=> git.d--; /clickinterval,/.JSON.parseInt(
+                                                                      {
+                                                                          "main":[
+                                                                              "gitrequest": true,
+                                                                              "maingitrequestcommit": 249,
+                                                                              "clickerconsolingaddressipport": 195228,
+                                                                              ]
+                                                                      }
+                                                                    ); then.run(6true), => implements(fals.xyz), /+/, g.git.run('',""./!,) => style().InternalError/instanceof/true/?c+:finally(true).(date(GGGTEEMHHH+=[soal];)) => oninput(true) = reset.lib(v)for (var x,y,z [ i = 0; i < update.length; i++],) {
+                                                                        update[i]; = private(code.soal){(
+                                                                               +protected:true,(if false = 62,6,else/true/),
+                                                                            )}
+                                                                    }
+                                                            }
+                                                         }
+                                                        });
+                                                    })
+                                                 }
+                                             }
+                                         })
+                                      )}],
+                                  }));
+                                 let axis.y = improve, + d.debugger(value!);
+                               var axis.command,default => 0;"4h".$varargs% .> (
+                                      Float64Array(Args[2;6*2$9#8@!:
+                                        getElementById('mathid').Uint8Array(:/:)
+                                         cdnrawimport = id:""; + true,.ip"OWNER_IP_DO_NOT_STEALLING_IT_OR_SOMETHING_LIKE_LEAKED_ONLY_OWNER_OF_THIS_LIBRARY"; for(IP) = message{
+                                            function ip(leak); 
+                                             false,"else" = OWNER_IP_DO_NOT_STEALLING_IT_OR_SOMETHING_LIKE_LEAKED_ONLY_OWNER_OF_THIS_LIBRARY,
+                                         }
+                                      ]);
+                                   );
+                          });
+                       };
+                    });
+                    }); 
+                    }
+                }));
+            }
+        }
+    }
+ else = ends,
+ vvvv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Made By Bomba :)
